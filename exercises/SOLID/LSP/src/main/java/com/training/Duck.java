0512871fe10b94b326ff159d77388bc8195828ce
Mauro Palsgraaf <mauropalsgraaf@hotmail.com>
2020-07-15 13:14:24 +0200

    Add LSP exercise

---
+++ exercises/SOLID/LSP/src/main/java/com/training/Duck.java
@@ -0,0 +1,14 @@
+package com.training;
+
+public class Duck
+{
+    public void quack()
+    {
+        System.out.println("Quack...");
+    }
+
+    public void swim()
+    {
+        System.out.println("Swim...");
+    }
+}
---
+++ exercises/SOLID/LSP/src/main/java/com/training/ElectronicDuck.java
@@ -0,0 +1,31 @@
+package com.training;
+
+public class ElectronicDuck extends Duck {
+    private boolean on = false;
+
+    @Override
+    public void quack() {
+        if (on) {
+            System.out.println("Electronic duck quack...");
+        } else {
+            throw new RuntimeException("Can't quack when off");
+        }
+    }
+
+    @Override
+    public void swim() {
+        if (on) {
+            System.out.println("Electronic duck swim");
+        } else {
+            throw new RuntimeException("Can't swim when off");
+        }
+    }
+
+    public void turnOn() {
+        on = true;
+    }
+
+    public void turnOff() {
+        on = false;
+    }
+}
